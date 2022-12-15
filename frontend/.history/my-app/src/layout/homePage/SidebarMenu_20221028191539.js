694a80725e9a8d0f90a996c987aef6ad331f0bb9
PhamTrungNguyen <trungnguyen010102hl@gmail.com>
2022-12-16 00:06:51 +0700

    pbl4

--- frontend/.history/my-app/src/layout/homePage/SidebarMenu_20221028191539.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SidebarMenu = () => {
-    return (
-        1231231
-    );
-};
-
-export default SidebarMenu;
--- frontend/.history/my-app/src/layout/homePage/SidebarMenu_20221028191545.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SidebarMenu = () => {
-    return (
-        <p>12312312312</p>
-    );
-};
-
-export default SidebarMenu;
