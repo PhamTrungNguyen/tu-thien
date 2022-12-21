2a8739a0952cc0afd47e678ce83f5be824f1ca1d
PhamTrungNguyen <trungnguyen010102hl@gmail.com>
2022-12-22 01:05:15 +0700

    update

--- pbl4.sql
+++ pbl4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 14, 2022 lúc 03:19 PM
+-- Thời gian đã tạo: Th12 21, 2022 lúc 07:04 PM
 -- Phiên bản máy phục vụ: 10.4.24-MariaDB
 -- Phiên bản PHP: 8.1.6
 
@@ -34,24 +34,25 @@ CREATE TABLE `accounts` (
   `roleId` int(11) DEFAULT NULL,
   `displayName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `totalMoney` varchar(255) COLLATE utf8_unicode_ci DEFAULT '0'
+  `totalMoney` varchar(255) COLLATE utf8_unicode_ci DEFAULT '0',
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `roleId`, `displayName`, `email`, `totalMoney`) VALUES
-(1, 'admin', '$2b$10$AGAs75F.yvblYlebE8vnROuMJBqJ5yuCyHsHWLWH3IDh2X74RzmTS', 3, NULL, NULL, NULL),
-(2, 'nguyen', '$2b$10$bnhKIIvvdm3pH4WbuWoYieJeQ9rnf5Q7j0xZs9.rbQZx8vP/qz1Eq', 2, 'Trung Nguyên', 'nguyen1234@gmail.com', '173017'),
-(4, 'thuyvy', '$2b$10$1RSrD9Jm8jBKSOnZgcwvQe8VzaW55oFDcCilfoDooNELGPGaj2gDi', 1, NULL, NULL, NULL),
-(5, 'hihi', '$2b$10$UcmW6fjz4Ee8.3GqprYJl.C.Z56zjaImnKpH4Z06Bd5qH8fmiXvjC', 2, 'Nguyễn Đình Thúy Vy', 'thuyvycva26@gmail.com', '100000'),
-(6, '123', '$2b$10$IClImDz32KlYnyUdakhPZ.qNAtSmlQZexrXkkrZDGvWlLvdHa.XHG', 1, NULL, NULL, '100000'),
-(7, 'nguyen12', '$2b$10$AabETzdMNzD6UWAs3gCoQekHrfHiQu63HnvKAFCrDPKm25XFDWvku', 2, 'Lai Thị Hồng Loan', NULL, '50000'),
-(8, '1234', '$2b$10$tLLlzP11wpFjaI1mC3v4O.2fq6YH3RjUVBvU71UKeSBM/ZvFhlfTK', 1, 'Test 1', NULL, '230000'),
-(9, 'ron', '$2b$10$4GUUOCns/ceeLGlzRvaoc.09n9QmPh88peGitmXKKgXcOBt2V5hZG', 2, 'Hồng Loan', 'loan@gmail.com', '0'),
-(10, 'test', '$2b$10$PkMfMjKl/1HIs69GQvkc0O3nfvlin5K6znTMomfSBO6kMgl3X0LGe', 1, NULL, NULL, '0'),
-(11, 'aaaa', '$2b$10$HaI.fK5fmlDxYNNMei4kCOLZ0U4cjbORLXxhGJURcDkwyipc1h86G', 1, NULL, NULL, '0');
+INSERT INTO `accounts` (`id`, `username`, `password`, `roleId`, `displayName`, `email`, `totalMoney`, `code`) VALUES
+(1, 'admin', '$2b$10$AGAs75F.yvblYlebE8vnROuMJBqJ5yuCyHsHWLWH3IDh2X74RzmTS', 3, NULL, NULL, NULL, NULL),
+(2, 'nguyen', '$2b$10$bnhKIIvvdm3pH4WbuWoYieJeQ9rnf5Q7j0xZs9.rbQZx8vP/qz1Eq', 2, 'Trung Nguyên', 'nguyen12345@gmail.com', '9378017', '12345'),
+(4, 'thuyvy', '$2b$10$1RSrD9Jm8jBKSOnZgcwvQe8VzaW55oFDcCilfoDooNELGPGaj2gDi', 1, NULL, NULL, NULL, NULL),
+(5, 'hihi', '$2b$10$UcmW6fjz4Ee8.3GqprYJl.C.Z56zjaImnKpH4Z06Bd5qH8fmiXvjC', 2, 'Nguyễn Đình Thúy Vy', 'thuyvycva26@gmail.com', '100000', NULL),
+(6, '123', '$2b$10$IClImDz32KlYnyUdakhPZ.qNAtSmlQZexrXkkrZDGvWlLvdHa.XHG', 1, NULL, NULL, '100000', NULL),
+(7, 'nguyen12', '$2b$10$AabETzdMNzD6UWAs3gCoQekHrfHiQu63HnvKAFCrDPKm25XFDWvku', 2, 'Nguyên 2', 'nguyen@gmail.com', '50000', '12345'),
+(8, '1234', '$2b$10$tLLlzP11wpFjaI1mC3v4O.2fq6YH3RjUVBvU71UKeSBM/ZvFhlfTK', 1, 'Test 1', NULL, '230000', NULL),
+(9, 'ron', '$2b$10$4GUUOCns/ceeLGlzRvaoc.09n9QmPh88peGitmXKKgXcOBt2V5hZG', 2, 'Hồng Loan', 'loan@gmail.com', '0', NULL),
+(10, 'test', '$2b$10$PkMfMjKl/1HIs69GQvkc0O3nfvlin5K6znTMomfSBO6kMgl3X0LGe', 1, NULL, NULL, '0', NULL),
+(11, 'aaaa', '$2b$10$HaI.fK5fmlDxYNNMei4kCOLZ0U4cjbORLXxhGJURcDkwyipc1h86G', 1, 'Nguyễn Văn A', 'abc@gmail.com', '0', NULL);
 
 -- --------------------------------------------------------
 
@@ -97,19 +98,17 @@ CREATE TABLE `donates` (
 --
 
 INSERT INTO `donates` (`id`, `money`, `userID`) VALUES
-(56, 300000, '7'),
-(57, 100000, '7'),
-(58, 100000, '2'),
 (59, 100000, '2'),
-(60, 50000, '8'),
 (61, 50000, '8'),
-(62, 200000, '9'),
-(63, 100000, '9'),
 (64, 100000, '9'),
 (65, 200000, '10'),
 (66, 100000, '9'),
 (67, 100000, '8'),
-(68, 100000, '8');
+(68, 100000, '8'),
+(69, 200000, '2'),
+(70, 200000, '2'),
+(71, 1500000, '2'),
+(72, 1000000, '7');
 
 -- --------------------------------------------------------
 
@@ -135,7 +134,9 @@ INSERT INTO `participants` (`id`, `userID`, `lead`, `postId`, `finish`) VALUES
 (23, 2, 0, 2, 1),
 (24, 11, 0, 5, 1),
 (48, 8, 1, 14, 0),
-(51, 2, 0, 14, 0);
+(51, 2, 0, 14, 0),
+(52, 11, 1, 30, 0),
+(53, 7, 1, 24, 0);
 
 -- --------------------------------------------------------
 
@@ -189,7 +190,11 @@ INSERT INTO `postdonates` (`createdAt`, `updatedAt`, `postId`, `donateId`) VALUE
 ('2022-12-02 13:04:55', '2022-12-02 13:04:55', 5, 64),
 ('2022-12-06 08:11:07', '2022-12-06 08:11:07', 5, 66),
 ('2022-12-10 16:21:02', '2022-12-10 16:21:02', 5, 68),
-('2022-12-10 16:16:31', '2022-12-10 16:16:31', 14, 67);
+('2022-12-10 16:16:31', '2022-12-10 16:16:31', 14, 67),
+('2022-12-20 18:34:21', '2022-12-20 18:34:21', 14, 69),
+('2022-12-21 16:45:03', '2022-12-21 16:45:03', 14, 70),
+('2022-12-21 16:48:53', '2022-12-21 16:48:53', 14, 71),
+('2022-12-21 17:55:54', '2022-12-21 17:55:54', 24, 72);
 
 -- --------------------------------------------------------
 
@@ -207,21 +212,24 @@ CREATE TABLE `posts` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   `accountId` int(11) DEFAULT NULL,
-  `totalParticipant` int(11) DEFAULT 5
+  `totalParticipant` int(11) DEFAULT 5,
+  `date` int(11) DEFAULT 30,
+  `totalMoney` double DEFAULT 2000000
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `posts`
 --
 
-INSERT INTO `posts` (`id`, `title`, `hot`, `status`, `desc`, `image`, `createdAt`, `updatedAt`, `accountId`, `totalParticipant`) VALUES
-(2, ' MẤT NGƯỜI THÂN, CHỈ CÒN CUỘC SỐNG NEO ĐƠN VỚI CĂN BỆNH TIM', 1, 3, 'Chồng mất sớm, đứa con gái duy nhất cũng qua đời khi mới 18 tuổi. Cuộc đời chịu quá nhiều đau thương, mất mát những người thân, căn bệnh suy tim ngày càng diễn biến xấu đi. Nhập viện cấp cứu và được chỉ định phẫu thuật nhưng chi phí quá lớn vượt ngoài khả năng, đã có lúc chị N tuyệt vọng muốn từ bỏ.\nChị Nguyễn T.H.N, sinh năm 1968 tại Bình Phước bị bệnh tim từ năm 18 tuổi nhưng chưa ảnh hưởng nhiều đến sức khỏe, Chị vẫn đi khám và uống thuốc duy trì. Chị lập gia đình và có 1 người con gái. Nhưng số phận trớ trêu, chồng chị mắc bệnh và qua đời sớm. Tất cả cuộc sống của chị chỉ còn lại cô con gái để nương tựa nhau trong cuộc sống nhưng một lần nữa, chị lại đón nhận sự mất mát gần như là tất cả, con gái chị qua đời khi chỉ 18 tuổi. Tinh thần suy sup, sức khỏe của chị lại càng ngày kém đi. Chị N được chẩn đoán suy tim, hở van 2 lá, hở van 3 lá và thường xuyên đến bệnh viện kiểm tra', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1668493883/pbl4/q8oygoj6pd9idtfzf5wt.png', '2022-11-15 06:31:28', '2022-12-10 14:05:56', 1, 5),
-(4, 'THA PHƯƠNG MƯU SINH GẶP TAI NẠN NGHIÊM TRỌNG', 0, 1, 'Lê T.L, sinh năm 2007, quê ở Huế, gia đình có 3 anh em, L vào Tp.Hồ Chí Minh làm việc chưa tròn một năm thì giữa tháng 9, em bị tai nạn giao thông trên đường đi làm về bị đa chấn thương. L được chẩn đoán chấn thương bụng, vỡ tạng. Mặc dù đã được cấp cứu qua cơn nguy kịch nhưng hiện tại em chỉ mới có lại nhận thức, vẫn phải thở máy, lọc máu, chưa ăn uống được, cần được chăm sóc đặc biệt. L đang được điều trị tại khoa Ngoại Bụng, Bệnh viện 175.\nGia đình ở quê thuộc diện khó khăn, ba mẹ không có công việc ổn định, ai thuê gì làm đó nuôi 3 anh em. Người anh lớn từ sớm đã vào Đồng Nai làm công nhân phụ giúp gia đình, L là người con thứ, nghỉ học từ năm lớp 7, ở nhà với ba mẹ đến nay thì muốn vào HCM tìm công việc mưu sinh. Một mình lặn lội từ miền Trung vào Sài Gòn với người cậu. L chỉ đi làm được vài tháng thì xảy ra chuyện không may. Những ngày đầu một mình người cậu chăm lo cho L trong khi chờ bố mẹ em vào.\nNghe tin con gặp tai nạn nghiêm trọng, tình trạng nguy kịch và chi phí điều trị vô cùng lớn với gia đình. Chị T-mẹ của bệnh nhân-thất thần nhưng cũng chạy đi vay mượn tiền để vào với con trai. Chắc hẳn chuyến xe đưa chị vào chăm con là chuyến xe xa nhất, lâu nhất và nặng nề nhất chị từng đi. Vì hoàn cảnh gia đình khó khăn nên các con không có điều kiện học hành buộc lòng các con phải ra đời bươn chải sớm. L có BHYT nhưng quá trình điều trị lâu dài, tình trạng chấn thương nghiêm trọng nên tính đến đầu tháng 10, gia đình vẫn đang nợ viện phí gần 30 triệu đồng. Gia đình đóng được 71 triệu tạm ứng hoàn toàn là tiền vay mượn. Ngày thường chi tiêu sinh hoạt đã thiếu trước, hụt sau thì nay tiền chục triệu là một con số vượt quá khả năng.', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1668494145/pbl4/cg15ju09o8oppco7emvm.png', '2022-11-15 06:35:58', '2022-11-15 06:35:58', 2, 5),
-(5, 'CÔ GIÁO MẦM NON PHẢI NGHỈ VIỆC VÌ BỆNH TẬT BỦA VÂY', 1, 3, 'Từng là giáo viên, từng là hiệu trưởng của một trường mầm non với hơn 20 năm công tác trong công việc dạy trẻ. Nhưng tai nạn bất ngờ trong lúc công tác đã cắt ngang con đường giảng dạy của chị. Đau đớn, hụt hẫng, bệnh tật bủa vây chị trong cùng một thời điểm, mất tất cả từ sự nghiệp đến sức khỏe, chị đã gần như tuyệt vọng.\nChị Nguyễn T.M.H, sinh năm 1979, tại Tp.Hồ Chí Minh. Vốn mang trong người căn bệnh thận phải ra vô bệnh viện theo dõi thường xuyên. Năm 2018, khi tổng vệ sinh tại trường, chị H bị mảnh gỗ đâm vào mắt dẫn đến nhiễm trùng, mặc dù đã cố gắng cứu chữa nhưng chị đã không thể giữ lại một bên mắt của mình. Chẳng bao lâu sau đó, căn bệnh thận của chị trở nặng, tay chân phù nề chị bước sang giai đoạn chạy thận. Di chứng từ bệnh thận dẫn đến chị bị đái tháo đường và làm cho một bên mắt còn lại của chị suy giảm nặng nề. Thị lực gần như mất đi, chị buộc lòng phải nghỉ việc. Sau một thời gian cố gắng làm thêm bên ngoài, chị đã chính thức không thể đứng lớp khi thị lực chỉ còn khoảng 20% và đươc duy trì bằng việc phải tiêm thuốc hàng tháng.\nCuộc sống của chị từ một giáo viên giỏi, cô hiệu trưởng tận tụy ngày ngày đến trường thì giờ đây chị phải đều đặn 3 ngày/tuần đến bệnh viện Hóc Môn để chạy thận, lọc máu. Số tiền dành dụm, tích góp sau bao năm đi làm vơi dần theo năm tháng vừa chạy thận, vừa điều trị mắt nhưng trong vô vọng. Ba mẹ chị đã ở tuổi 70 không còn sức lao động do bệnh đau và tai nạn giao thông, cả nhà đang ở nhờ trong căn nhà của người họ hàng. Giờ đây, tất cả chi phí sinh hoạt, khám bệnh hàng tháng đều được người quen và em gái hỗ trợ.', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1668494196/pbl4/kwttg839s5wfk0csvwze.png', '2022-11-15 06:36:41', '2022-12-10 17:10:11', 2, 3),
-(14, 'Người đàn bà lực điền', 1, 2, '“Chỉ vài tháng sau khi tôi bắt đầu tập luyện, tôi đã đi trình diễn ở các trận đấu trong vùng và tôi nhận ra rằng tôi đã rất thích nó. Tôi đã trở thành một nhà vô địch bất bại của giải Zabaikalye Region khi tôi chỉ 16 tuổi. Đó là cách mọi thứ bắt đầu. Tôi thậm chí đã từng tham gia một trận chiến đối kháng khốc liệt. Dù rằng tôi không dành chiến thắng, nhưng kết quả thật sự không quá tệ. Song, những kết quả sẽ bị lãng quên một cách nhanh chóng thôi”.', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1669429433/pbl4/ipalvaswzsqeia1dkkmt.jpg', '2022-11-26 02:24:00', '2022-12-10 17:26:20', 5, 5),
-(24, '123', 0, 1, '123', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1669658175/pbl4/qk7actnl0hrudqa7oix4.jpg', '2022-11-28 17:56:19', '2022-11-28 17:56:19', 7, 5),
-(26, 'test_11111111', 0, 1, '1111111111111111111', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1669787151/pbl4/ms3yyst4ttgdx6z7paad.jpg', '2022-11-30 05:46:01', '2022-11-30 05:46:01', 2, 5),
-(29, 'aaaaaa', 1, 1, 'aaaaaaaa', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1670310978/pbl4/yuusjfaayuseigjvthow.png', '2022-12-06 07:16:25', '2022-12-10 14:18:08', 9, 5);
+INSERT INTO `posts` (`id`, `title`, `hot`, `status`, `desc`, `image`, `createdAt`, `updatedAt`, `accountId`, `totalParticipant`, `date`, `totalMoney`) VALUES
+(2, ' MẤT NGƯỜI THÂN, CHỈ CÒN CUỘC SỐNG NEO ĐƠN VỚI CĂN BỆNH TIM', 1, 3, 'Chồng mất sớm, đứa con gái duy nhất cũng qua đời khi mới 18 tuổi. Cuộc đời chịu quá nhiều đau thương, mất mát những người thân, căn bệnh suy tim ngày càng diễn biến xấu đi. Nhập viện cấp cứu và được chỉ định phẫu thuật nhưng chi phí quá lớn vượt ngoài khả năng, đã có lúc chị N tuyệt vọng muốn từ bỏ.\nChị Nguyễn T.H.N, sinh năm 1968 tại Bình Phước bị bệnh tim từ năm 18 tuổi nhưng chưa ảnh hưởng nhiều đến sức khỏe, Chị vẫn đi khám và uống thuốc duy trì. Chị lập gia đình và có 1 người con gái. Nhưng số phận trớ trêu, chồng chị mắc bệnh và qua đời sớm. Tất cả cuộc sống của chị chỉ còn lại cô con gái để nương tựa nhau trong cuộc sống nhưng một lần nữa, chị lại đón nhận sự mất mát gần như là tất cả, con gái chị qua đời khi chỉ 18 tuổi. Tinh thần suy sup, sức khỏe của chị lại càng ngày kém đi. Chị N được chẩn đoán suy tim, hở van 2 lá, hở van 3 lá và thường xuyên đến bệnh viện kiểm tra', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1668493883/pbl4/q8oygoj6pd9idtfzf5wt.png', '2022-11-15 06:31:28', '2022-12-10 14:05:56', 1, 5, 30, 2000000),
+(4, 'THA PHƯƠNG MƯU SINH GẶP TAI NẠN NGHIÊM TRỌNG', 1, 1, 'Lê T.L, sinh năm 2007, quê ở Huế, gia đình có 3 anh em, L vào Tp.Hồ Chí Minh làm việc chưa tròn một năm thì giữa tháng 9, em bị tai nạn giao thông trên đường đi làm về bị đa chấn thương. L được chẩn đoán chấn thương bụng, vỡ tạng. Mặc dù đã được cấp cứu qua cơn nguy kịch nhưng hiện tại em chỉ mới có lại nhận thức, vẫn phải thở máy, lọc máu, chưa ăn uống được, cần được chăm sóc đặc biệt. L đang được điều trị tại khoa Ngoại Bụng, Bệnh viện 175.\nGia đình ở quê thuộc diện khó khăn, ba mẹ không có công việc ổn định, ai thuê gì làm đó nuôi 3 anh em. Người anh lớn từ sớm đã vào Đồng Nai làm công nhân phụ giúp gia đình, L là người con thứ, nghỉ học từ năm lớp 7, ở nhà với ba mẹ đến nay thì muốn vào HCM tìm công việc mưu sinh. Một mình lặn lội từ miền Trung vào Sài Gòn với người cậu. L chỉ đi làm được vài tháng thì xảy ra chuyện không may. Những ngày đầu một mình người cậu chăm lo cho L trong khi chờ bố mẹ em vào.\nNghe tin con gặp tai nạn nghiêm trọng, tình trạng nguy kịch và chi phí điều trị vô cùng lớn với gia đình. Chị T-mẹ của bệnh nhân-thất thần nhưng cũng chạy đi vay mượn tiền để vào với con trai. Chắc hẳn chuyến xe đưa chị vào chăm con là chuyến xe xa nhất, lâu nhất và nặng nề nhất chị từng đi. Vì hoàn cảnh gia đình khó khăn nên các con không có điều kiện học hành buộc lòng các con phải ra đời bươn chải sớm. L có BHYT nhưng quá trình điều trị lâu dài, tình trạng chấn thương nghiêm trọng nên tính đến đầu tháng 10, gia đình vẫn đang nợ viện phí gần 30 triệu đồng. Gia đình đóng được 71 triệu tạm ứng hoàn toàn là tiền vay mượn. Ngày thường chi tiêu sinh hoạt đã thiếu trước, hụt sau thì nay tiền chục triệu là một con số vượt quá khả năng.', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1668494145/pbl4/cg15ju09o8oppco7emvm.png', '2022-11-15 06:35:58', '2022-12-15 17:05:38', 2, 5, 30, 2000000),
+(5, 'CÔ GIÁO MẦM NON PHẢI NGHỈ VIỆC VÌ BỆNH TẬT BỦA VÂY', 1, 3, 'Từng là giáo viên, từng là hiệu trưởng của một trường mầm non với hơn 20 năm công tác trong công việc dạy trẻ. Nhưng tai nạn bất ngờ trong lúc công tác đã cắt ngang con đường giảng dạy của chị. Đau đớn, hụt hẫng, bệnh tật bủa vây chị trong cùng một thời điểm, mất tất cả từ sự nghiệp đến sức khỏe, chị đã gần như tuyệt vọng.\nChị Nguyễn T.M.H, sinh năm 1979, tại Tp.Hồ Chí Minh. Vốn mang trong người căn bệnh thận phải ra vô bệnh viện theo dõi thường xuyên. Năm 2018, khi tổng vệ sinh tại trường, chị H bị mảnh gỗ đâm vào mắt dẫn đến nhiễm trùng, mặc dù đã cố gắng cứu chữa nhưng chị đã không thể giữ lại một bên mắt của mình. Chẳng bao lâu sau đó, căn bệnh thận của chị trở nặng, tay chân phù nề chị bước sang giai đoạn chạy thận. Di chứng từ bệnh thận dẫn đến chị bị đái tháo đường và làm cho một bên mắt còn lại của chị suy giảm nặng nề. Thị lực gần như mất đi, chị buộc lòng phải nghỉ việc. Sau một thời gian cố gắng làm thêm bên ngoài, chị đã chính thức không thể đứng lớp khi thị lực chỉ còn khoảng 20% và đươc duy trì bằng việc phải tiêm thuốc hàng tháng.\nCuộc sống của chị từ một giáo viên giỏi, cô hiệu trưởng tận tụy ngày ngày đến trường thì giờ đây chị phải đều đặn 3 ngày/tuần đến bệnh viện Hóc Môn để chạy thận, lọc máu. Số tiền dành dụm, tích góp sau bao năm đi làm vơi dần theo năm tháng vừa chạy thận, vừa điều trị mắt nhưng trong vô vọng. Ba mẹ chị đã ở tuổi 70 không còn sức lao động do bệnh đau và tai nạn giao thông, cả nhà đang ở nhờ trong căn nhà của người họ hàng. Giờ đây, tất cả chi phí sinh hoạt, khám bệnh hàng tháng đều được người quen và em gái hỗ trợ.', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1668494196/pbl4/kwttg839s5wfk0csvwze.png', '2022-11-15 06:36:41', '2022-12-21 17:21:07', 2, 2, 10, 1000000),
+(14, 'Người đàn bà lực điền', 1, 2, '“Chỉ vài tháng sau khi tôi bắt đầu tập luyện, tôi đã đi trình diễn ở các trận đấu trong vùng và tôi nhận ra rằng tôi đã rất thích nó. Tôi đã trở thành một nhà vô địch bất bại của giải Zabaikalye Region khi tôi chỉ 16 tuổi. Đó là cách mọi thứ bắt đầu. Tôi thậm chí đã từng tham gia một trận chiến đối kháng khốc liệt. Dù rằng tôi không dành chiến thắng, nhưng kết quả thật sự không quá tệ. Song, những kết quả sẽ bị lãng quên một cách nhanh chóng thôi”.', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1669429433/pbl4/ipalvaswzsqeia1dkkmt.jpg', '2022-11-26 02:24:00', '2022-12-10 17:26:20', 5, 5, 30, 2000000),
+(24, '123', 0, 2, '123', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1669658175/pbl4/qk7actnl0hrudqa7oix4.jpg', '2022-11-28 17:56:19', '2022-12-21 18:03:11', 7, 3, 13, 2000000),
+(26, 'test_11111111', 0, 1, '1111111111111111111', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1669787151/pbl4/ms3yyst4ttgdx6z7paad.jpg', '2022-11-30 05:46:01', '2022-11-30 05:46:01', 2, 5, 30, 2000000),
+(29, '2222222222222', 0, 1, '2222222222222', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1671119701/pbl4/sypkeihn7zcgbahpi0qm.gif', '2022-12-06 07:16:25', '2022-12-15 15:55:05', 9, 4, 30, 2000000),
+(30, 'wwwwwwwwwwwww', 1, 2, 'wwwwwwwwwwwwwww', 'http://res.cloudinary.com/drx9ycs46/image/upload/v1671122183/pbl4/hfopjuwonlzsnuyt5ydo.png', '2022-12-15 16:05:06', '2022-12-15 16:36:28', 2, 4, 30, 2000000);
 
 -- --------------------------------------------------------
 
@@ -322,13 +330,13 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT cho bảng `donates`
 --
 ALTER TABLE `donates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT cho bảng `participants`
 --
 ALTER TABLE `participants`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT cho bảng `postcomments`
@@ -340,7 +348,7 @@ ALTER TABLE `postcomments`
 -- AUTO_INCREMENT cho bảng `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT cho bảng `roles`
@@ -368,8 +376,8 @@ ALTER TABLE `participants`
 -- Các ràng buộc cho bảng `postcomments`
 --
 ALTER TABLE `postcomments`
-  ADD CONSTRAINT `postcomments_ibfk_239` FOREIGN KEY (`commentId`) REFERENCES `comments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `postcomments_ibfk_240` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `postcomments_ibfk_395` FOREIGN KEY (`commentId`) REFERENCES `comments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `postcomments_ibfk_396` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Các ràng buộc cho bảng `postdonates`
